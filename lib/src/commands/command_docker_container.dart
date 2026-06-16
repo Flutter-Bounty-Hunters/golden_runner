f5448f6ffe5c4699b5f9247bd8e3e1df2bec3f9e
Matt Carroll <matthew-carroll@users.noreply.github.com>
2026-06-16 12:49:54 -0700

    Make more stuff protected

--- lib/src/commands/command_docker_container.dart
+++ lib/src/commands/command_docker_container.dart
@@ -13,14 +13,17 @@ abstract class DockerContainerCommand implements Command {
   static const defaultDockerVerbosity = DockerVerbosity.errorOnly;
 
   @visibleForTesting
+  @protected
   String get dockerFilePath => _dockerFilePath!;
   String? _dockerFilePath;
 
   @visibleForTesting
+  @protected
   String get dockerImageName => _dockerImageName!;
   String? _dockerImageName;
 
   @visibleForTesting
+  @protected
   DockerVerbosity get dockerVerbosity => _dockerVerbosity!;
   DockerVerbosity? _dockerVerbosity;
 
@@ -28,23 +31,27 @@ abstract class DockerContainerCommand implements Command {
   /// to alter the host file system.
   ///
   /// Defaults to nothing.
+  @protected
   Set<String> get mountPaths => {};
 
   /// The path from where this command is running, to the directory that should be copied over into the
   /// Docker Image.
   ///
   /// Defaults to ".", which copies content from where this command is run.
+  @protected
   String get pathToProjectRoot => ".";
 
   /// The path within the Docker Container where the [command] should be run.
   ///
   /// Defaults to ".", which runs the command within the root directory of what's copied to the Docker Image.
+  @protected
   String get containerWorkingDirectory => ".";
 
   /// Returns the CLI command that should run in the Docker Container that's setup by this command.
   ///
   /// It's expected that this command will require information from [parseArguments], so it's OK for
   /// implementers to throw an error if this is ever accessed before [parseArguments] is called.
+  @protected
   List<String> get command;
 
   @override
